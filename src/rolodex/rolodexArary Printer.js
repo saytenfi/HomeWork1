8bf1dd793d0a73a11845ff6c050fc27fe9f3c1e2
samuelAytenfisu <sayten@uw.edu>
2021-04-18 09:41:53 -0700

    'rolodexPrinter Array'

---
+++ src/rolodex/rolodexArary Printer.js
@@ -0,0 +1,10 @@
+import people from './people.json';
+
+
+people.forEach((person) => {
+  const names = person.name.split(' ');
+  const [firstName, lastName] = names;
+  const { email, phone } = person;
+
+  console.log(`First name: ${firstName}\nLast name: ${lastName}\nEmail: ${email}\nPhone number: ${phone}\n`);
+});
